8b8f3512149d3445c07a7d76927ab85d59a7e2ce
albyschianni <alberto.schianni@gmail.com>
2022-12-22 20:52:28 +0100

    aggiunta sezione principale footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -10,16 +10,63 @@ export default{
 </script>
 
 <template>
-    <section>
+    <section class="footer-main">
+        
+        <div class="container-up">
+            <div class="get-touch">
+                <span>SEND A MESSAGE</span>
+                <div class="h2-touch">Get in Touch</div>
+                <p>We will respond to your message as soon as possible.</p>
+                
+                <div class="input-val">
+                    <input class="design-input" type="text" placeholder="Name">
+                    <input class="design-input" type="email" placeholder="Email">
+                    <input class="design-input" type="number" placeholder="Phone">
+                    <select class="design-input" name="More Info" id="More Info" placeholder="More Info" style="color:#6b6b6b">
+                        <option value="" disabled selected>More info</option>
+                        <option value="">A</option>
+                        <option value="">B</option>
+                    </select>
+                    <input class="message" type="text" placeholder="Message">
+                </div>
+
+                <a id="send" href="">SEND</a>
+
+            </div>
+            <div class="info-industry">
+                <div class="agency-name">
+                    Example Inc.
+                </div>
+                <p id="lorem-up">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <p>Pariatur ducimus consequatur voluptas illum ullam laborum possimus.</p>
+                
+                <div id="contact">
+                    <div style="color: #098080;" class="item-contact">
+                        <font-awesome-icon icon="fa-solid fa-phone" /> +1 (305) 1234-5678
+                    </div>
+                    <div style="color: #098080;" class="item-contact">
+                        <font-awesome-icon icon="fa-solid fa-envelope" /> hello@example.com
+                    </div>
+                    <div style="color: #098080;" class="item-contact">
+                        <font-awesome-icon icon="fa-solid fa-location-dot" /> Main Avenue, 987
+                    </div>
+                </div>
+                
+
+                <a id="map" href="">VIEW MAP</a>
+            </div>
+         </div>
+        
 
     </section>
+
     <section id="lower-footer">
-        <div class="container">
+        <div class="container-low">
             <div class="text-low-foot">
                 Enjoy the low price. We are tracking any intention of piracy.
             </div>
             <div class="text-low-foot">
-                2020 NEXGEN is Proudly Powered by <a id="link-foot" href="">Codings</a>
+                <font-awesome-icon icon="fa-regular fa-copyright" /> 2020 NEXGEN is Proudly Powered by <a id="link-foot" href="">Codings</a>
             </div>
         </div>
     </section>
@@ -28,11 +75,110 @@ export default{
 <style lang="scss" scoped>
 @use '../style/general.scss';
 
-.container {
+.footer-main {
+    background-color: #ffffff;
+}
+.container-up {
+    max-width: 1170px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    padding-top: 100px;
+    padding-bottom: 100px;
+}
+.get-touch {
+    width: 60%;
+}
+.info-industry {
+    width: 30%;
+    padding-right: 40px;
+}
+.input-val {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+.design-input {
+    width: 49%;
+    padding: 15px;
+    height: 50px;
+    border: none;
+    border-radius: 4px;
+    background-color: #ececec;
+    margin-bottom: 10px;
+}
+.message {
+ width: 100%;
+ height: 150px;
+ background-color: #ececec;
+ border: none;
+ border-radius: 4px;
+ padding: 15px;
+ margin-bottom: 30px;
+ 
+}
+#send {
+    background-color: #098080;
+    text-decoration: none;
+    color: white;
+    padding: 10px 15px;
+    border-radius: 3px;
+}
+span {
+    color: #098080;
+    font-size: 14px;
+}
+.h2-touch {
+    font-size: 40px;
+    color: #21333e;
+    font-weight: bold;
+    margin: 20px 0px;
+}
+p {
+    font-size: 15px;
+    color: #756d75;
+    margin-bottom: 40px;
+}
+#map {
+    text-decoration: none;
+    color: #098080;
+    border: 1px solid #098080;
+    padding: 10px 15px;
+    border-radius: 3px;
+}
+.agency-name{
+    color: #21333e;
+    font-size: 22px;
+    font-weight: bold;
+    margin-bottom: 20px;
+}
+#lorem-up {
+    margin-bottom: 20px;
+}
+#contact {
+    margin-bottom: 30px;
+}
+.fa-phone, .fa-envelope, .fa-location-dot{
+    background-color: #daeced;
+    padding: 7px;
+    border-radius: 50%;
+    margin-right: 10px;
+}
+.item-contact {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+
+}
+
+// seconda sezione
+.container-low {
     display: flex;
     justify-content: space-between;
     align-items: center;
     min-height: 50px;
+    max-width: 1170px;
+    margin: 0 auto;
 }
 #lower-footer {
     background-color: #28363c;
--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
-import { faGem, faUser } from '@fortawesome/free-regular-svg-icons'
-import { faArrowRight, faClock, faEnvelope, faLocation, faMotorcycle, faPhone, faRocket, faSuitcase, faTruckFast, faTruckMoving, faVanShuttle } from '@fortawesome/free-solid-svg-icons'
+import { faCopyright, faGem, faUser } from '@fortawesome/free-regular-svg-icons'
+import { faArrowRight, faClock, faEnvelope, faLocationDot, faMotorcycle, faPhone, faRocket, faSuitcase, faTruckFast, faTruckMoving, faVanShuttle } from '@fortawesome/free-solid-svg-icons'
 
 
 
-library.add( faFacebook, faTwitter, faLinkedin, faUser, faEnvelope, faPhone, faClock, faLocation, faTruckFast, faMotorcycle, faVanShuttle, faTruckMoving, faRocket, faSuitcase,faGem, faArrowRight )
+library.add( faFacebook, faTwitter, faLinkedin, faUser, faEnvelope, faPhone, faClock, faLocationDot, faTruckFast, faMotorcycle, faVanShuttle, faTruckMoving, faRocket, faSuitcase,faGem, faArrowRight, faCopyright )
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
